f7f6e767205117113f233ce81e07dd4f2440544a
louis nguyen <louis.nguyen550@gmail.com>
2025-07-26 16:50:49 -0400

    fix small error with url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { useState } from 'react';
 import MovieCard from "./components/MovieCard";
 
-const API_URL = "http://www.omdbapi.com?apikey=a68daac8";
+const API_URL = "https://www.omdbapi.com?apikey=a68daac8";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
